9304868085735a9f0f34d77d83ca5d505206ca78
Pankaj Parkar <parkarpankaj3@gmail.com>
2019-08-19 01:19:23 +0530

    Collected input element from the shadowDOM

--- src/app/wc-adapter.directive.ts
+++ src/app/wc-adapter.directive.ts
@@ -1,10 +1,33 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef } from '@angular/core';
+import { ControlValueAccessor } from '@angular/forms';
 
 @Directive({
-  selector: '[wafWcAdapter]'
+  selector: '[wafWcAdapter][formControl],[wafWcAdapter][formControlName]'
 })
-export class WcAdapterDirective {
+export class WcAdapterDirective implements ControlValueAccessor {
 
-  constructor() { }
+  constructor(private el: ElementRef) { }
 
+  onChange = (_) => {};
+  onTouched = () => {};
+
+  writeValue(obj: any): void {
+    this.onChange(obj);
+  }
+  registerOnChange(fn: any): void {
+    this.onTouched = fn;
+  }
+  registerOnTouched(fn: any): void {
+    this.onChange = fn;
+  }
+
+  ngOnInit () {
+    const element = this.el.nativeElement;
+    const input = element.localName;
+    const shadow = element.shadowRoot;
+    console.log(this.el, 'Element ', input === 'input' ? 
+      element:
+      shadow.querySelector('input')
+    )
+  }
 }
