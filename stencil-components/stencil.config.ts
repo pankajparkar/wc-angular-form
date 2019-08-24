d6c2d8e90b7fcac1065d0a0ac33dfa1422b44222
Pankaj Parkar <parkarpankaj3@gmail.com>
2019-08-24 21:28:40 +0530

    Added stencil project with sample component.

---
+++ stencil-components/stencil.config.ts
@@ -0,0 +1,18 @@
+import { Config } from '@stencil/core';
+
+export const config: Config = {
+  namespace: 'stencil-components',
+  outputTargets: [
+    {
+      type: 'dist',
+      esmLoaderPath: '../loader'
+    },
+    {
+      type: 'www',
+      serviceWorker: null // disable service workers
+    }
+  ],
+  bundles: [
+    {components: ['my-component']}
+  ]
+};
