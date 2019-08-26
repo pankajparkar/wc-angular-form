08a1117630a1b6fcc0b7416ba119d58420a69ad9
Pankaj Parkar <parkarpankaj3@gmail.com>
2019-08-26 23:47:44 +0530

    Added hashLocation strategy.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,22 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, LocationStrategy, HashLocationStrategy } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { NgformComponent } from './ngform/ngform.component';
+import { PolymerComponent } from './polymer/polymer.component';
+import { WebcomponentComponent } from './webcomponent/webcomponent.component';
 
 const routes: Routes = [
   {path: 'ngform', component: NgformComponent},
-  {path: 'polymer', component: NgformComponent},
-  {path: 'webcomponent', component: NgformComponent},
-  {path: '**', redirectTo: 'ngform'},
+  {path: 'polymer', component: PolymerComponent},
+  {path: 'webcomponent', component: WebcomponentComponent},
+  {path: '', redirectTo: 'ngform', pathMatch: 'full'},
 ]
 
 @NgModule({
   declarations: [],
+  providers: [
+    {provide: LocationStrategy, useClass: HashLocationStrategy}
+  ],
   imports: [
     CommonModule,
     RouterModule.forRoot(routes, {useHash: true})
