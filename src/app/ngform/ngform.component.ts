88d80db75643af57aa42632c61e003a182ead972
Pankaj Parkar <parkarpankaj3@gmail.com>
2019-10-04 23:59:28 +0530

    Added form controls.

--- src/app/ngform/ngform.component.ts
+++ src/app/ngform/ngform.component.ts
@@ -10,7 +10,10 @@ export class NgformComponent implements OnInit {
 
   form = this.fb.group({
     firstName: new FormControl('', [Validators.required]),
-    lastName: new FormControl('4', [Validators.required]),
+    lastName: new FormControl('', [Validators.required]),
+    address: new FormControl('', [Validators.required]),
+    gender: new FormControl('4', [Validators.required]),
+    test: new FormControl('4', [Validators.required]),
   })
 
   constructor(private fb: FormBuilder) {
